4f8ebc28858e56f7fb4854e13fa550eeda0f3900
Jan Zeleny <jzeleny@redhat.com>
2015-08-23 18:56:20 +0200

    Get rid of local dependencies
    Download then from Maven Central instead.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CLASSPATH="target/srvs-0.1.jar:lib/*:lib2/*"
+CLASSPATH="target/pis-1.0.jar:lib/*"
 
 #java -verbose:gc -Xms1000m -Xmx1500m -Dtx.useLog=false -cp $CLASSPATH org.fit.psc.Main $@
 java -Xms1200m -Xmx1700m -Dtx.useLog=false -cp $CLASSPATH org.fit.pis.Main $@
